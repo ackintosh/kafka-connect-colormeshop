050eb839408317495305cce120a945d2986e4a78
akihito.nakano <sora.akatsuki@gmail.com>
2017-09-13 09:37:34 +0900

    generate template from archtype

---
+++ src/test/java/com/github/ackintosh/kafka/connect/MySourceTaskTest.java
@@ -0,0 +1,10 @@
+package com.github.ackintosh.kafka.connect;
+
+import org.junit.Test;
+
+public class MySourceTaskTest {
+  @Test
+  public void test() {
+    // Congrats on a passing test!
+  }
+}
