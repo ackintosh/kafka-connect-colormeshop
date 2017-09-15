b45f91f301bcf3dea6b0be1185f96c29f99c281b
akihito.nakano <sora.akatsuki@gmail.com>
2017-09-15 09:48:48 +0900

    Implementing the http client

--- src/main/java/com/github/ackintosh/kafka/connect/ColormeShopAPIHttpClient.java
+++ src/main/java/com/github/ackintosh/kafka/connect/ColormeShopAPIHttpClient.java
@@ -1,7 +1,31 @@
 package com.github.ackintosh.kafka.connect;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
+import org.json.JSONArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ColormeShopAPIHttpClient {
+    private static final Logger log = LoggerFactory.getLogger(ColormeShopAPIHttpClient.class);
+
     public ColormeShopAPIHttpClient() {
 
     }
+
+    protected JSONArray getNextSales() throws InterruptedException {
+        GetRequest unirest = Unirest.get("https://api.shop-pro.jp/v1/sales.json");
+        log.debug(String.format("GET %s", unirest.getUrl()));
+
+        try {
+            HttpResponse<JsonNode> response = unirest.asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            return new JSONArray();
+        }
+    }
+
 }
