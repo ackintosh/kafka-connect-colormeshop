480310e960134836a326a03928a8750201b94a10
akihito.nakano <sora.akatsuki@gmail.com>
2017-10-01 13:34:39 +0900

    Add http client test

---
+++ src/test/java/com/github/ackintosh/kafka/connect/ColormeShopAPIHttpClientTest.java
@@ -0,0 +1,34 @@
+package com.github.ackintosh.kafka.connect;
+
+import com.github.ackintosh.kafka.connect.model.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColormeShopAPIHttpClientTest {
+    @Test
+    public void getNextSales() {
+        String accessToken = System.getProperty("access_token");
+        if (accessToken == null) {
+            Assert.fail();
+        }
+
+        Map<String, String> map = new HashMap();
+        map.put("access_token", accessToken);
+        ColormeShopSourceConnectorConfig config = new ColormeShopSourceConnectorConfig(map);
+        ColormeShopAPIHttpClient client = new ColormeShopAPIHttpClient(config);
+
+        try {
+            Response response = client.getNextSales(
+                    LocalDateTime.now().minusDays(7).toInstant(ZoneOffset.UTC)
+            );
+            Assert.assertTrue(response.getMeta().getInt("total") > 0);
+        } catch (InterruptedException e) {
+            Assert.fail();
+        }
+    }
+}
